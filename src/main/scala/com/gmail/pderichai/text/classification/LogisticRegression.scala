6a61082978e102e828dbee3569ba4115115f96db
Erika Wolfe <eywolfe@cs.uw.edu>
2016-11-14 20:02:54 +0100

    Logistic regression check in

--- src/main/scala/com/gmail/pderichai/text/classification/LogisticRegression.scala
+++ src/main/scala/com/gmail/pderichai/text/classification/LogisticRegression.scala
@@ -1,22 +1,29 @@
 package com.gmail.pderichai.text.classification
+import breeze.linalg
+import breeze.linalg.SparseVector
 
 /**
   * Created by dericp on 11/14/16.
   */
+
+//    val vec = breeze.linalg.SparseVector.zeros[Integer](5)
+//    vec(2) = 1
+//    vec(2) += 2
+//    println("VECTOR: " + vec)
+
 object LogisticRegression {
-
-  case class SMap (val m: Map[String,Double])
-    extends AnyVal {
-    def *(other: SMap) : Double =
-      m.map{case (k,v) => v*other.m.getOrElse(k,0.0)}.sum
-    def *(scalar: Double) : SMap =
-      SMap(m.mapValues(v => v * scalar))}
-
-  def logistic(x: SMap, y: SMap) : Double =
-    1.0 / (1.0 + Math.exp(-(x*y)))
-
-  def update(th: SMap, x: SMap, c: Boolean) = {
-    val z = if (c) (1-logistic(th,x)) else (-logistic(th,x))
-    x * z
+  // Returns value that we have to subtract from theta to get new theta
+  def updateTheta(thetaVec: SparseVector[Double], docVec: SparseVector[Double], docIsInCategory: Boolean, step: Int): SparseVector[Double] = {
+    val z = if (docIsInCategory) (1-logistic(thetaVec,docVec)) else (-logistic(thetaVec,docVec))
+    //println("doc term freqs: " + docVec)
+    //println("update: " + z)
+    //println("subtract: " + (docVec * z))
+    //docSMap.termFreqs.keySet.filter(!theta.termFreqs.keySet.contains(_))
+    //docSMap.termFreqs.foreach{ case (k, v) => theta.termFreqs(k) = theta.termFreqs.getOrElse(k, 0.0) - v}
+    thetaVec - ((docVec * z) * (1.0 / step))
   }
+
+  def logistic(docVec: SparseVector[Double], thetaVec: SparseVector[Double]) : Double =
+  // 1.0 / (1.0 + Math.exp(-1 * (docVec.dot(thetaVec)) - b))
+    1.0 / (1.0 + Math.exp(-1 * (docVec.dot(thetaVec))))
 }
