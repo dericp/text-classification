d667bc189de94a89c39e50cb78a9946e9ff2cc21
Erika Wolfe <eywolfe@cs.uw.edu>
2016-11-15 10:19:21 +0100

    Erika Naive Bayes

--- src/main/scala/com/gmail/pderichai/text/classification/Main.scala
+++ src/main/scala/com/gmail/pderichai/text/classification/Main.scala
@@ -6,15 +6,13 @@ import scala.collection.mutable
 import scala.collection.mutable.ListBuffer
 
 object Main {
-  val STOP_WORDS = Set("and", "but", "of", "from", "by", "because", "he", "she", "if", "a", "about")
-
   def main(args: Array[String]): Unit = {
     // Map: docID -> document
     def docs = new ReutersRCVStream("src/main/resources/train").stream
 //    val iter = docs.iterator
 //    val doc1 = iter.next()
 //    val doc2 = iter.next()
-    val codesToDocIDs = scala.collection.mutable.Map.empty[String, mutable.Seq[Int]].filter(_._2.size > 20)
+    val codesToDocIDs = scala.collection.mutable.Map.empty[String, mutable.Seq[Int]].filter(_._2.size > 40)
     val docIDToDoc = scala.collection.mutable.Map.empty[Int, Document]
     val vocabSize = docIDToDoc.values.map(_.tokens).toSet.size
 
@@ -25,89 +23,69 @@ object Main {
       }
     }
 
-//    println("found codes" + NaiveBayes.catsGivenDoc(docIDToDoc, doc1.ID, -400, codesToDocIDs))
-//    println("correct codes" + doc1.codes)
-//    println()
-//    println("found codes" + NaiveBayes.catsGivenDoc(docIDToDoc, doc2.ID, -400, codesToDocIDs))
-//    println("correct codes" + doc2.codes)
-
-
-    // PREV: f1 score: 0.43158146636129013 : not using utils stop words, taking 45, deleting cats with freq < 10
-    // f1 score: 0.4096063471839869 : using utils stop words, taking 40, deleting cats with < 20
-
     println("finished training")
 
     def validationDocs = new ReutersRCVStream("src/main/resources/validation").stream
-
-
-
-    // val missingTermRates = Set(-40.0, -50.0, -60.0, -30.0, -80.0, -100.0, -20.0)  WINNER = -50
-    // val thresholds = Set(-495, -500, -505, -510, -490)  WINNER = -510
     val f1Vals = ListBuffer.empty[Double]
     val naiveBayes = new NaiveBayes(docIDToDoc, codesToDocIDs, vocabSize, -50)
     var i = 0
     var prevLen = 0;
+
     for (doc <- validationDocs) {
-      if (i <= 100) {
-        val foundCats = naiveBayes.catsGivenDoc(shortenContent(doc), -510)
+      if (i <= 300) {
+        val foundCats = naiveBayes.catsGivenDoc(shortenContent(doc), -600)
         val correctCats = doc.codes
         val score = docF1Score(foundCats, correctCats)
         println("i = " + i + ", score = " + score)
         f1Vals += score
-        if (f1Vals.size == prevLen) {
-          println("!!! Size did not change!!! f1Vals = " + f1Vals +  ", correct cats = " + correctCats + ", found cats = " + foundCats)
-        }
-        if (score.isNaN) {
-          println("NaN!  foundCats = " + foundCats.size + ", correctCats = " + correctCats)
-        }
-        // println("Added docF1Score to f1Vals. i = " + i + ", docF1Score =  " + score + ", " + "f1Vals size = " + f1Vals.size)
-        // println()
         i += 1
         prevLen = f1Vals.size
-        // if (i % 100 == 0) println("i = " + i)
         println()
       }
-
     }
-    // Why is sum of algF1Score (should be # entries) sometimes less than # docs looked at??
     println()
     println("f1 score: " + algF1Score(f1Vals))
     println("finished f1vals = " + f1Vals)
   }
 
+  // Takes an XMLDocument doc
+  // Returns a Document representing doc
   def shortenContent(doc: XMLDocument): Document = {
     val termFreqs = termFreq(doc)
     return new Document(termFreqs, doc.codes, doc.tokens.filter(keepWord(_, termFreqs)), doc.ID)
   }
 
+  // Takes an XMLDocument
+  // Returns the term frequency map of the document, deleting stop words and keeping only the top k
+  // TODO: top k?
   def termFreq(doc: XMLDocument): Map[String, Int] = {
-    // val tokens = Tokenizer.tokenize(doc.content)
     // Utils.getTermFrequencies(doc).toList.sortBy{_._2:_*}.take(45)
     collection.immutable.ListMap(doc.tokens.filter(!Utils.STOP_WORDS.contains(_)).groupBy(identity)
-      .mapValues(l => l.length).toList.sortBy{_._2}:_*).take(40)
+      .mapValues(l => l.length).toList.sortBy{-_._2}:_*).take(60)
   }
 
+  // Takes a String s and the term frequencies for a document
+  // Returns whether or not the doc should store the word
   def keepWord(s: String, termFreqs: Map[String, Int]): Boolean = {
     val stops = !Utils.STOP_WORDS.contains(s)
-    val termsss = termFreqs.keySet.contains(s)
-    return stops && termsss
+    val terms = termFreqs.keySet.contains(s)
+    return stops && terms
   }
 
+  // Takes the Set of categories for this doc found by our algorithm and the
+  // Set of categories that are correct for this doc
+  // Returns F1 score for doc = 2PR / (P + R)
   def docF1Score(foundCats: scala.collection.Set[String], correctCats: Set[String]): Double = {
-    // Precision = # relevant items retrieved / # items retrieved
-    // Recall = # relevant items retrieved / # relevant items in collection
-    // F1 = 2PR / (P + R)
-    println("foundCats size: " + foundCats.size)
+    // println("foundCats size: " + foundCats.size)
     val relevantRetrieved = foundCats.filter(correctCats(_)).size.toDouble
-    if (relevantRetrieved == 0) {
-      return 0
-    }
-
+    if (relevantRetrieved == 0) return 0
     val p = relevantRetrieved / foundCats.size
     val r = relevantRetrieved / correctCats.size
     2 * p * r / (p + r)
   }
 
+  // Takes the Set of f1 scores for a group of documents
+  // Returns average f1 score
   def algF1Score(docScores: ListBuffer[Double]): Double = {
     docScores.sum / docScores.size
   }
--- src/main/scala/com/gmail/pderichai/text/classification/NaiveBayes.scala
+++ src/main/scala/com/gmail/pderichai/text/classification/NaiveBayes.scala
@@ -3,24 +3,25 @@ import ch.ethz.dal.tinyir.processing.{Tokenizer, XMLDocument}
 
 import scala.collection.mutable
 // Currently: f1 = 0.41434146680965195
-// TODO: Cache probabilities that are computed multiple times, stop storing tokens in Document, always return < 5 categories
-// TODO: Use n-fold validation (maybe not)
+// TODO: stop storing tokens in Document
 // What do we do with the validation set?  What do we do with words that are in the validation set but not the training set?
 class NaiveBayes(docs: mutable.Map[Int, Document], cats: mutable.Map[String, mutable.Seq[Int]], vocabSize: Int, missingTermWeight: Double) {
   val numTrainingDocs = docs.keySet.size.toDouble
   val catToWordProbs = mutable.Map.empty[String, Map[String, Double]]
+  // val totalNumTokens = docs.map(_._2.tokens).sum
+  // val allTermFreq = docs.map(_._2.tokens).flatten.toSeq.groupBy(identity).mapValues(l => l.size + 1)
 
   // Returns the probability of a given category
-  def probC(cat: String): Double = {
-    cats.getOrElse(cat, Seq.empty).size / numTrainingDocs
-  }
+  def probC(cat: String): Double = cats.getOrElse(cat, Seq.empty).size / numTrainingDocs
+
+  def probNotC(cat: String): Double = 1 - probC(cat)
 
   // Returns a map from word w --> probability of w in the given category
   def probOfWGivenCMany(cat: String): Map[String, Double] = {
     if (catToWordProbs.contains(cat)) {
       catToWordProbs(cat)
     } else {
-      val tks = cats.getOrElse(cat, Seq.empty).map(docs.get(_).get.tokens).flatten
+      val tks = cats.getOrElse(cat, Seq.empty).flatMap(docs(_).tokens)
       val denominator = tks.length.toDouble + vocabSize
       val result = tks.groupBy(identity).mapValues(l => Math.log((l.length + 1) / denominator))
       catToWordProbs(cat) = result
@@ -28,36 +29,61 @@ class NaiveBayes(docs: mutable.Map[Int, Document], cats: mutable.Map[String, mut
     }
   }
 
+  // TODO: Make sure - doesn't change cats
+  // def probOfWGivenNotCMany(cat: String): Map[String, Double] = {
+//    // all tokens in docs - tokens from docs that are in category
+//    // val tks = allTokens.diff(cats(cat).map(docs(_).tokens).flatten)
+//    val catTF = cats(cat).flatMap(docs(_).tokens).map(l => l.length + 1)
+//    allTermFreq.map{case(term, freq) => (term, (freq - catTF(term)))}
+//    // tks = all
+//    val denominator = tks.size.toDouble + vocabSize
+//    val result = tks.groupBy(identity).mapValues(l => Math.log((l.size + 1) / denominator))
+//    // catToWordProbs(cat) = result
+//    result
+    // remove cat
+    // cats.keySet.map(probOfWGivenCMany())
+  // }
+
   def probOfDocGivenCat(probOfWGivenCMany: Map[String, Double], doc: Document, cat: String): Double = {
     val probOfC = probC(cat)
     val termFreq = doc.termFreq
     // termFreq.getOrElse(term, 0) --> _.get
-    Math.log(probOfC) + termFreq.map{case(term, i)=>(termFreq.getOrElse(term, 0) * probOfWGivenCMany.getOrElse(term, missingTermWeight))}.sum
+    Math.log(probOfC) + termFreq.map{case(term, i)=>termFreq.getOrElse(term, 0) * probOfWGivenCMany.getOrElse(term, missingTermWeight)}.sum
   }
 
-  def test(abc: Set[String]): Unit = {}
+//  def probOfDocGivenNotCat(probOfWgivenNotCMany: Map[String, Double], doc: Document, cat: String): Double = {
+//    println("hey")
+//    val probOfNotC = probNotC(cat)
+//    val termFreq = doc.termFreq
+//    Math.log(probOfNotC) + termFreq.map{case(term, i)=>(termFreq.getOrElse(term, 0) * probOfWgivenNotCMany.getOrElse(term, missingTermWeight))}.sum
+//  }
+
+  // def test(abc: Set[String]): Unit = {}
 
   def catsGivenDoc(doc: Document, threshold: Double): collection.Set[String] = {
+    // cats.keySet.filter{case(cat) => probOfDocGivenCat(probOfWGivenCMany(cat), doc, cat) > probOfDocGivenNotCat(probOfWGivenNotCMany(cat), doc, cat) }
+
+
     val catProbs = cats.keySet.zipWithIndex.map{case (cat, i) => (cat, probOfDocGivenCat(probOfWGivenCMany(cat), doc, cat))}.toMap
     // println("catProbs" + catProbs.values)
     val thresholdProbs = catProbs.filter{case (cat, prob) => prob >= threshold}
     //println("thresholdProbs")
     // Currently only returns the top answer because thresholdProbs is empty
-    if (thresholdProbs.size > 50) {
-      // modifying > 40, taking top 40 docs f1 score: 0.3836954505053356
-      // modifying > 50, taking top 50 docs f1 score: 0.38489195313432195
-      // modifying > 50, taking top 20 docs f1 score:
-      thresholdProbs.toSeq.sortBy(_._2).take(20).map{case(k, v) => k}.toSet
-    } else
-    if (thresholdProbs.size > 0) {
-      val toReturn = thresholdProbs.map(_._1).toSet
-      // println("using thresholdProbs: " + toReturn)
-      toReturn
-    } else  {
-      val toReturn = Set(catProbs.maxBy(_._2)._1)
-      // println("nothing > threshold" + toReturn)
-      toReturn
-    }
+//    if (thresholdProbs.size > 5) {
+//      val toReturn = thresholdProbs.toSeq.sortBy(-_._2).take(4).map{case(k, v) => k}.toSet
+//      println("shortened thresholdProbs: " + toReturn)
+//      toReturn
+//    } else if (thresholdProbs.size > 0) {
+//      val toReturn = thresholdProbs.map(_._1).toSet
+//      println("using thresholdProbs: " + toReturn)
+//      toReturn
+//    } else {
+//      val toReturn = catProbs.toSeq.sortBy(-_._2).take(4).map{case(k, v) => k}.toSet
+//      println("nothing > threshold: " + toReturn)
+//      toReturn
+//    }
+    // f1 score: 0.5993730359276583
+    catProbs.toSeq.sortBy(-_._2).take(4).map{case(k, v) => k}.toSet
   }
 
   //  def topCodeForDoc(cats: mutable.Map[String, mutable.Seq[Int]], docs: mutable.Map[Int, Document], docID: Int): String = {
